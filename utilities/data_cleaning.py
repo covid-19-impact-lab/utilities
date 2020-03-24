6c32c06203a9e5998eca418410bf5365ac1b595e
roecla <klara.roehrl@gmail.com>
2020-03-24 09:16:49 +0100

    Fix errors in table found with the data_cleaning functions.

--- utilities/data_cleaning.py
+++ utilities/data_cleaning.py
@@ -16,7 +16,23 @@ from pandas.api.types import is_categorica
         data (pd.DataFrame): DataFrame satisfying the 
     
     """
-    pass
+    dict_data = description.set_index('new_name')[["type", "categories_english", "ordered"]]
+    overlap = set(data.columns).intersection(dict_data.index)
+    dict_data = dict_data.loc[overlap]
+    col_to_dtype = dict_data.T.to_dict()
+    for col, col_dict in col_to_dtype.items():
+        dtype = col_dict["type"]
+        if dtype != "Categorical": 
+            try:
+                data[col] = data[col].astype(dtype)
+            except:
+                print(col, dtype, data[col].unique())
+        else:
+            pass
+            # cats = col_dict["categories_english"]
+            # ordered = col_dict["ordered"]
+            # data[col] = pd.Categorical(values=data[col], categories=cats, ordered=ordered)
+    return data
 
 
 def check_description(data, data_name, description, logging="print"):
@@ -33,7 +49,7 @@ def check_description(data, data_name, description, logging="print"):
     _check_types(types=description["type"], logging=logging)
     _check_var_overlap_btw_description_and_data(
         data_vars=data.columns,
-        descr_vars=description[data_name],
+        covered=description[data_name],
         logging=logging,
     )
 
@@ -42,10 +58,10 @@ def check_description(data, data_name, description, logging="print"):
 
 
 def _check_new_names(new_names, logging):
-    if not new_names.duplicated().any(): 
-        msg = "The new name column should not contain duplicates."
+    if new_names.duplicated().any(): 
+        msg = f"{new_names[new_names.duplicated()]} are duplicates in the new_name column."
         _custom_logging(msg=msg, logging=logging)
-    if not new_names.isnull().any(): 
+    if new_names.isnull().any(): 
         msg = "The new name column should not contain NaNs"
         _custom_logging(msg=msg, logging=logging)
 
@@ -62,7 +78,7 @@ def _check_types(types, logging):
         msg = "You should declare a type for every variable."
         _custom_logging(msg=msg, logging=logging)
 
-    known_types = ["bool", "int", "float", "Categorical", "str"]
+    known_types = ["boolean", "Int64", "float", "Categorical", "str"]
     if not types.isin(known_types).all():
         unknown_types = types[~types.isin(known_types)].tolist()
         msg = "There are unknokn types: \n" + "\n\t".join(unknown_types)
@@ -70,13 +86,13 @@ def _check_types(types, logging):
 
 
 def _check_var_overlap_btw_description_and_data(data_vars, covered, logging):
-    missing_in_description = [x for x in data_vars if x not in descr_vars]
+    missing_in_description = [x for x in data_vars if x not in covered]
     if len(missing_in_description) > 0:
         msg = "The following variables from the raw dataset are not " + \
             "covered by the description table: \n\t" + \
             "\n\t".join(missing_in_description)
         _custom_logging(msg=msg, logging=logging)
-    missing_in_data = [x for x in descr_vars if x not in data_vars]
+    missing_in_data = [x for x in covered if x not in data_vars]
     if len(missing_in_data) > 0:
         msg = "The following variables from the description table are not " + \
             "in the raw dataset: \n\t" + \
@@ -101,8 +117,10 @@ def _check_categorical_cols(description, data_name, logging):
 
 
 def _load_description(path):
-    description = pd.read_csv(
-        path, true_values=["TRUE"], false_values=["FALSE"])
+    description = pd.read_csv(path)
+    description.replace({"TRUE": True, "FALSE": False}, inplace=True)
+    # use nullable pandas dtypes
+    description["type"].replace({"int": "Int64", "bool": "boolean"}, inplace=True)
     description.dropna(how='all', axis=0, inplace=True)
     description.dropna(how='all', axis=1, inplace=True)
     return description
