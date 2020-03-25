84ee87d20134b9dbac7540847b3b66346a6da5ab
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-03-25 15:11:46 +0100

    Include package data.

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from setuptools import setup
 setup(
     name="utilities",
     version="0.0.1",
-    description="Package with code that has to be shared across repos of covid-19-impact-lab",
+    description=(
+        "Package with code that has to be shared across repos of covid-19-impact-lab"
+    ),
     license="MIT",
     url="https://github.com/covid-19-impact-lab/covidlab-utilities",
     author="COVID-19 Impact Lab",
     author_email="janos.gabler@gmail.com",
     packages=find_packages(),
     zip_safe=False,
-    # package_data={"estimagic": ["optimization/algo_dict.json"]},
-    # include_package_data=True,
+    package_data={
+        "utilities": ["covid19_data_description.csv", "test_data_cleaning.ipynb"]
+    },
+    include_package_data=True,
 )
