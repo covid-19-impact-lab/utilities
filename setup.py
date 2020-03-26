0a3e54215745e730d41efa6c8fc7a509a2901b2a
Klara Roehrl <klara.roehrl@gmail.com>
2020-03-26 13:31:43 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="utilities",
-    version="0.0.2",
+    version="0.0.3",
     description=(
         "Package with code that has to be shared across repos of covid-19-impact-lab"
     ),
