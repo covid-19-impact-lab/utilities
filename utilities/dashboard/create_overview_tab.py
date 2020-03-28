76964556a550028aad1bc872907fd65a62af0175
Klara Roehrl <klara.roehrl@gmail.com>
2020-03-28 10:07:16 +0100

    Switch to Select Widget for Topic and Subtopic.

--- utilities/dashboard/create_overview_tab.py
+++ utilities/dashboard/create_overview_tab.py
@@ -3,8 +3,7 @@ from functools import partial
 from auxiliary import create_standard_figure
 from bokeh.layouts import Column
 from bokeh.layouts import Row
-from bokeh.models import ColumnDataSource
-from bokeh.models import Dropdown
+from bokeh.models import Select
 from bokeh.models import Panel
 from bokeh.models import RadioButtonGroup
 from bokeh.models.widgets import Div
@@ -16,10 +15,11 @@ def create_overview_tab(
     topic_to_groups,
     group_to_header,
     group_to_plot_type,
-    background_vars,
+    background_variables,
     plot_data,
     group_to_variables,
     variable_to_label,
+    variable_to_nice_name,
 ):
     """Create the overview tab showing the distribution of any group of variables.
 
@@ -29,10 +29,11 @@ def create_overview_tab(
         topic_to_groups (dict)
         group_to_header (dict)
         group_to_plot_type (dict)
-        background_vars (list)
+        background_variables (list)
         plot_data (dict)
         group_to_variables (dict)
         variable_to_label (dict)
+        variable_to_nice_name (dict)
 
     Returns:
         tab (bokeh.models.Tab)
@@ -41,37 +42,39 @@ def create_overview_tab(
     title = Div(text="Plot Title", style={"font-size": "200%"})
 
     # setup the elements
-    topic_selector = Dropdown(label="Topic", menu=topics, name="topic_dd")
-    subtopic_selector = Dropdown(
-        label="Subtopic", menu=["choose a topic first"], name="group_dd",
+    topic = topics[0]
+    topic_selector = Select(title="Topic:", options=topics, value=topic, name="topic_selector")
+    subtopics = topic_to_groups[topic]
+    subtopic_selector = Select(
+        title="Subtopic:", options=subtopics, value=subtopics[0], name="subtopic_selector",
     )
-    background_selector = RadioButtonGroup(labels=background_vars, active=0)
+    background_selector = RadioButtonGroup(labels=background_variables, active=0)
     widgets = Row(topic_selector, subtopic_selector, background_selector)
 
     header = Div(text="Choose a topic and subtopic to start!", name="header")
     canvas = create_standard_figure(title="", name="canvas")
 
-    # add the callbacks
-    topic_callback = partial(
-        topic_handler,
-        topic_selector=topic_selector,
-        topic_to_groups=topic_to_groups,
-        subtopic_selector=subtopic_selector,
-    )
-    topic_selector.on_click(topic_callback)
-    subtopic_callback = partial(
-        subtopic_handler,
-        canvas=canvas,
-        group_to_plot_type=group_to_plot_type,
-        plot_data=plot_data,
-        header=header,
-        group_to_header=group_to_header,
-    )
-    subtopic_selector.on_click(subtopic_callback)
-    background_var_callback = partial(
-        background_var_handler, background_vars=background_vars
-    )
-    background_selector.on_click(background_var_callback)
+    # # add the callbacks
+    # topic_callback = partial(
+    #     topic_handler,
+    #     topic_selector=topic_selector,
+    #     topic_to_groups=topic_to_groups,
+    #     subtopic_selector=subtopic_selector,
+    # )
+    # topic_selector.on_click(topic_callback)
+    # subtopic_callback = partial(
+    #     subtopic_handler,
+    #     canvas=canvas,
+    #     group_to_plot_type=group_to_plot_type,
+    #     plot_data=plot_data,
+    #     header=header,
+    #     group_to_header=group_to_header,
+    # )
+    # subtopic_selector.on_click(subtopic_callback)
+    # background_var_callback = partial(
+    #     background_var_handler, background_variables=background_variables
+    # )
+    # background_selector.on_click(background_var_callback)
 
     col = Column(title, widgets, header, canvas)
     tab = Panel(child=col, title="Overview", name="overview_panel")
@@ -95,10 +98,9 @@ def subtopic_handler(
     if old_glyph is not None:
         canvas.renderers.remove(old_glyph)
 
-    source = ColumnDataSource(plot_data[new_group]["all"])
-    canvas.line(x="var1", y="var2", name="glyph", source=source)
+    canvas.line(x=[1, 2, 3], y=[4, 5, 6], name="glyph")
 
 
-def background_var_handler(new, background_vars):
-    new_var = background_vars[new]
+def background_var_handler(new, background_variables):
+    new_var = background_variables[new]
     print(f"\n\nNew background variable: {new_var}\n\n")
