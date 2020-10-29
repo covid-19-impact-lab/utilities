88d41038387301252805ba7366df5c7c99c30a45
Janos Gabler <janos.gabler@gmail.com>
2020-10-29 17:34:07 +0100

    Start to extract map into separate component.

--- utilities/dashboard/app/create_overview_tab.py
+++ utilities/dashboard/app/create_overview_tab.py
@@ -77,6 +77,206 @@ def assemble_dashboard_components(
         groupby_title=groupby_title,
     )
 
+    map_page = create_maps(
+        groups=groups,
+        topics=topics,
+        topic_to_groups=topic_to_groups,
+        group_to_header=group_to_header,
+        group_to_plot_type=group_to_plot_type,
+        background_variables=background_variables,
+        plot_data=plot_data,
+        map_data=map_data,
+        group_to_variables=group_to_variables,
+        variable_to_label=variable_to_label,
+        variable_to_nice_name=variable_to_nice_name,
+        group_to_caption=group_to_caption,
+        title=title,
+        top_text=top_text,
+        plot_intro=plot_intro,
+        groupby_title=groupby_title,
+        nice_name_to_variable=nice_name_to_variable,
+        menu_titles=menu_titles,
+        nth_str=nth_str,
+        tab_names=tab_names,
+    )
+
+    group_to_nicenames = {}
+    for g, variables in group_to_variables.items():
+        group_to_nicenames[g] = [variable_to_nice_name[var] for var in variables]
+    # start values
+    topic = topics[0]
+    subtopics = topic_to_groups[topic]
+    group = subtopics[0]
+    var_nice_name = group_to_nicenames[group][0]
+    plot_type = group_to_plot_type[group]
+    setup_plot = getattr(plot_modules[plot_type], "setup_plot")
+
+    # map elements
+    map_selectors = [
+        Select(
+            title=menu_titles[0],
+            options=topics,
+            value=topic,
+            name="topic_selector",
+            width=220,
+        ),
+        Select(
+            title=menu_titles[1],
+            options=subtopics,
+            value=group,
+            name="subtopic_selector",
+            width=220,
+        ),
+        Select(
+            title=menu_titles[3],
+            options=group_to_nicenames[group],
+            value=group_to_nicenames[group][0],
+            width=160,
+        ),
+    ]
+    map_func = partial(setup_map, map_data=map_data)
+    country_map = map_func(group=group, var_nice_name=var_nice_name)
+
+    create_caption = partial(
+        _create_caption,
+        group_to_header=group_to_header,
+        group_to_caption=group_to_caption,
+        group_to_variables=group_to_variables,
+        variable_to_nice_name=variable_to_nice_name,
+        variable_to_label=variable_to_label,
+    )
+
+    plot_selectors = [
+        Select(
+            title=menu_titles[0],
+            options=topics,
+            value=topic,
+            name="topic_selector",
+            width=200,
+        ),
+        Select(
+            title=menu_titles[1],
+            options=subtopics,
+            value=group,
+            name="subtopic_selector",
+            width=250,
+        ),
+        Select(
+            title=menu_titles[2],
+            options=[nth_str] + background_variables,
+            value=nth_str,
+            width=100,
+        ),
+    ]
+
+    plot = setup_plot(**plot_data[group], bg_var=nth_str, nth_str=nth_str)  # noqa
+    plot_caption = create_caption(group=group)
+    bg_info = Div(text="", margin=(10, 0, 10, 0), style=HEADER_STYLE)
+
+    plot_page = Column(
+        # plot_title, plot_intro,
+        Row(*plot_selectors),
+        plot,
+        plot_caption,
+        bg_info,
+    )
+
+    # plot callbacks
+    topic_callback = partial(
+        _set_lower_vals, high_to_lower=topic_to_groups, lower_selector=plot_selectors[1]
+    )
+    plot_selectors[0].on_change("value", topic_callback)
+
+    subtopic_callback = partial(
+        set_subtopic,
+        plot_data=plot_data,
+        page=plot_page,
+        background_selector=plot_selectors[2],
+        group_to_plot_type=group_to_plot_type,
+        caption_callback=create_caption,
+        nth_str=nth_str,
+    )
+    plot_selectors[1].on_change("value", subtopic_callback)
+
+    background_var_callback = partial(
+        condition_on_background_var,
+        subtopic_selector=plot_selectors[1],
+        plot_data=plot_data,
+        page=plot_page,
+        group_to_plot_type=group_to_plot_type,
+        variable_to_label=variable_to_label,
+        group_to_variables=group_to_variables,
+        nice_name_to_variable=nice_name_to_variable,
+        nth_str=nth_str,
+    )
+    plot_selectors[2].on_change("value", background_var_callback)
+
+    page = Tabs(
+        tabs=[
+            Panel(child=intro_page, title=tab_names[0]),
+            Panel(child=map_page, title=tab_names[1]),
+            Panel(child=plot_page, title=tab_names[2]),
+        ]
+    )
+    return page
+
+
+def create_maps(
+    groups,
+    topics,
+    topic_to_groups,
+    group_to_header,
+    group_to_plot_type,
+    background_variables,
+    plot_data,
+    map_data,
+    group_to_variables,
+    variable_to_label,
+    variable_to_nice_name,
+    group_to_caption,
+    title,
+    top_text,
+    plot_intro,
+    groupby_title,
+    nice_name_to_variable,
+    menu_titles,
+    nth_str,
+    tab_names,
+):
+    """Create the overview tab showing the distribution of any group of variables.
+
+    Args:
+        groups (list)
+        topics (list)
+        topic_to_groups (dict)
+        group_to_header (dict)
+        group_to_plot_type (dict)
+        background_variables (list)
+        plot_data (dict)
+        group_to_variables (dict)
+        variable_to_label (dict)
+        variable_to_nice_name (dict)
+        nice_name_to_variable (dict)
+        group_to_caption (dict)
+        title (str)
+        groupby_title (str)
+        top_text (str)
+        plot_intro (str)
+        menu_titles (tuple)
+        nth_str (str): name of the "Nothing" category in English
+
+    Returns:
+        page (bokeh Column)
+
+    """
+
+    intro_page = create_intro_page(
+        title=title,
+        top_text=top_text,
+        plot_intro=plot_intro,
+        groupby_title=groupby_title,
+    )
+
     group_to_nicenames = {}
     for g, variables in group_to_variables.items():
         group_to_nicenames[g] = [variable_to_nice_name[var] for var in variables]
@@ -194,14 +394,7 @@ def assemble_dashboard_components(
     )
     plot_selectors[2].on_change("value", background_var_callback)
 
-    page = Tabs(
-        tabs=[
-            Panel(child=intro_page, title=tab_names[0]),
-            Panel(child=map_page, title=tab_names[1]),
-            Panel(child=plot_page, title=tab_names[2]),
-        ]
-    )
-    return page
+    return map_page
 
 
 def _add_map_callbacks(
