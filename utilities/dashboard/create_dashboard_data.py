92c92c57c07e6c0edb7a3c564434867402513688
Janos Gabler <janos.gabler@gmail.com>
2020-10-31 12:44:35 +0100

    Disentangle data creation.

--- utilities/dashboard/create_dashboard_data.py
+++ utilities/dashboard/create_dashboard_data.py
@@ -44,6 +44,58 @@ def create_dashboard_data(
 
     """
 
+    shared_data = _create_shared_dashboad_data(
+        data=data,
+        data_desc=data_desc,
+        group_info=group_info,
+        language=language,
+        data_name=data_name,
+    )
+    variable_mappings = shared_data["variable_mappings"]
+    menu_labels = shared_data["menu_labels"]
+
+    groups = _get_groups(group_info, language)
+
+    nice_names = data_desc.set_index("new_name")[f"nice_name_{language}"].to_dict()
+
+    # create map data =================================================================
+    maps_data = create_maps_data(
+        data=data,
+        variable_mappings=variable_mappings,
+        nice_names=nice_names,
+        groups=groups,
+        language=language,
+        data_name=data_name,
+    )
+
+    # dist data =======================================================================
+
+    univariate_distributions_data = create_univariate_distributions_data(
+        data=data,
+        variable_mappings=variable_mappings,
+        nice_names=nice_names,
+        groups=groups,
+        group_info=group_info,
+        menu_labels=menu_labels,
+        language=language,
+    )
+    # joining ====================================
+    res = {}
+    res["shared_data"] = shared_data
+    res["intro_page_data"] = create_intro_page_data(language, data_name)
+    res["univariate_distributions_data"] = univariate_distributions_data
+    res["maps_data"] = maps_data
+    return res
+
+
+def _get_groups(group_info, language):
+    raw_groups = group_info[f"group_{language}"].unique().tolist()  # noqa
+    bg_var_groups = ["Background Overview", "Background Correlation"]
+    groups = [group for group in raw_groups if group not in bg_var_groups]
+    return groups
+
+
+def _create_shared_dashboad_data(data, data_desc, group_info, language, data_name):
     vm = create_general_variable_mappings(
         data=data,
         data_desc=data_desc,
@@ -51,22 +103,17 @@ def create_dashboard_data(
         language=language,
         data_name=data_name,
     )
-    groups = _get_groups(group_info, language)
-
-    internal_bg_vars = vm["group_to_variables"]["Background Overview"]
 
     menu_labels = get_menu_labels(language)
-    res = {}
     shared_data = {
         "language": language,
         "variable_mappings": vm,
         "menu_labels": menu_labels,
     }
+    return shared_data
 
-    res["shared_data"] = shared_data
 
-    nice_names = data_desc.set_index("new_name")[f"nice_name_{language}"].to_dict()
-
+def create_maps_data(data, variable_mappings, nice_names, groups, language, data_name):
     if language == "english":
         translations = {
             "Province": "Province",
@@ -92,17 +139,27 @@ def create_dashboard_data(
     for g in groups:
         maps_data[g] = prepare_maps_data(
             data=data,
-            variables=vm["group_to_variables"][g],
+            variables=variable_mappings["group_to_variables"][g],
             nice_names=nice_names,
-            labels=vm["variable_to_label"],
+            labels=variable_mappings["variable_to_label"],
             data_name=data_name,
         )
 
-    univariate_distributions_data = {}
+    return maps_data
+
+
+def create_univariate_distributions_data(
+    data, variable_mappings, nice_names, groups, group_info, menu_labels, language
+):
+    vm = variable_mappings
+
+    relevant_bg_vars = vm["group_to_variables"]["Background Overview"]
+
+    res = {}
     group_to_plot_type = group_info.set_index(f"group_{language}")[
         "plot_type"
     ].to_dict()
-    univariate_distributions_data["group_to_plot_type"] = group_to_plot_type
+    res["group_to_plot_type"] = group_to_plot_type
 
     plot_data = {}
 
@@ -112,26 +169,15 @@ def create_dashboard_data(
         plot_data[g] = prepare_data(
             data=data,
             variables=vm["group_to_variables"][g],
-            bg_vars=[x for x in internal_bg_vars if x != "prov"],
+            bg_vars=[x for x in relevant_bg_vars if x != "prov"],
             nice_names=nice_names,
             labels=vm["variable_to_label"],
             nothing_string=menu_labels["nothing_category"],
         )
 
-    univariate_distributions_data["plot_data"] = plot_data
-    univariate_distributions_data["background_variables"] = [
-        nice_names[var] for var in internal_bg_vars if var != "prov"
+    res["plot_data"] = plot_data
+    res["background_variables"] = [
+        nice_names[var] for var in relevant_bg_vars if var != "prov"
     ]
 
-    res["intro_page_data"] = create_intro_page_data(language, data_name)
-
-    res["univariate_distributions_data"] = univariate_distributions_data
-    res["maps_data"] = maps_data
     return res
-
-
-def _get_groups(group_info, language):
-    raw_groups = group_info[f"group_{language}"].unique().tolist()  # noqa
-    bg_var_groups = ["Background Overview", "Background Correlation"]
-    groups = [group for group in raw_groups if group not in bg_var_groups]
-    return groups
