d2a461a3630435a52d6d37dd33d163578857204a
Klara Roehrl <klara.roehrl@gmail.com>
2021-01-07 10:36:03 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print(p)
 
 setup(
     name="utilities",
-    version="0.3.7",
+    version="0.3.8",
     description=(
         "Package with code that has to be shared across repos of covid-19-impact-lab"
     ),
